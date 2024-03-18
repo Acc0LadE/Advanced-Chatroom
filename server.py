1511e758ebe63b303e235a6411071709c42be7f1
gk__ <142667881+Acc0LadE@users.noreply.github.com>
2024-03-18 13:32:13 +0530

    update kick funcrtion for admin command

--- server.py
+++ server.py
@@ -18,8 +18,18 @@ def broadcast(message):
 def handle(client):
     while True:
         try:
-            message = client.recv(1024)
-            broadcast(message)
+            msg = message = client.recv(1024)
+            if msg.decode('ascii').startswith('KICK'):
+                name_to_kick = msg.decode('ascii')[5:]
+                kick_user(name_to_kick)
+            elif msg.decode('ascii').startswith("BAN"):
+                name_to_ban = msg.decode('ascii')[4:]
+                kick_user(name_to_ban)
+                with open('bans.txt', 'a') as f:
+                    f.write(f'{name_to_ban}\n')
+                print(f'{name_to_ban} was banned.')
+            else:
+                broadcast(message)
 
         except:
             index = clients.index(client)
@@ -58,6 +68,16 @@ def receive():
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
+
+def kick_user(name):
+    if name in nicknames:
+        name_index = nicknames.index(name)
+        client_to_kick = clients[name_index]
+        clients.remove(client_to_kick)
+        client_to_kick.send('You were kicked by an admin.'.encode('ascii'))
+        client_to_kick.close()
+        nicknames.remove(name)
+        broadcast(f'{name} was kicked by an admin'.encode('ascii'))
 
 print("[+] Server is listening......")
 receive()
